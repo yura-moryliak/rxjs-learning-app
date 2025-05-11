1eda9f97aafa531591f07fe884bc4121e199560d
ymrlk <y.moryliak@loginvsi.com>
2025-05-11 18:06:01 +0300

    Add user status tracking with timer-based state changes
    Implemented `BehaviorSubject` objects to track user status (online/offline, active/inactive) and updated the UI using `combineLatest`. Added timer-based subscriptions for managing status transitions and interactive restart functionality. Enhanced styling for status indicators.

--- src/app/structure/combinational-operators/combine-latest/tasks/combine-latest-task-3/combine-latest-task-3.component.ts
+++ src/app/structure/combinational-operators/combine-latest/tasks/combine-latest-task-3/combine-latest-task-3.component.ts
@@ -1,11 +1,14 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Subscription} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
+import {BehaviorSubject, map, Observable, Subscription, take, timer, combineLatest} from 'rxjs';
 import {TaskOverviewComponent} from '../../../../../common/task-overview/task-overview.component';
+
 
 @Component({
   selector: 'app-combine-latest-task-3',
   imports: [
-    TaskOverviewComponent
+    TaskOverviewComponent,
+    AsyncPipe
   ],
   templateUrl: './combine-latest-task-3.component.html',
   styleUrl: './combine-latest-task-3.component.scss'
@@ -13,9 +16,125 @@ import {TaskOverviewComponent} from '../../../../../common/task-overview/task-ov
 export class CombineLatestTask3Component implements OnInit, OnDestroy {
   private readonly subscription: Subscription = new Subscription();
 
-  ngOnInit(): void {}
+  private onlineSubscription: Subscription = new Subscription();
+  private offlineSubscription: Subscription = new Subscription();
+
+  private activeSubscription: Subscription = new Subscription();
+  private inactiveSubscription: Subscription = new Subscription();
+
+  onlineTimer$: Observable<number> = getTimer();
+  offlineTimer$: Observable<number> = getTimer(0, 2000);
+
+  activeTimer$: Observable<number> = getTimer(0, 2000);
+  inactiveTimer$: Observable<number> = getTimer();
+
+  userOnline$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  userActive$: BehaviorSubject<boolean> = new BehaviorSubject(false);
+
+  onlineOfflineTimerValue: number | undefined;
+  activeInactiveTimerValue: number | undefined;
+
+  isUserOnline: boolean = false;
+  isUserActive: boolean  = false;
+
+  isStatusChanged: boolean | undefined;
+  showRestartButton: boolean | undefined;
+
+  ngOnInit(): void {
+    this.startTimers();
+  }
+
+  startTimers(): void {
+    this.startOnlineTimer();
+    this.startActiveTimer();
+
+    const statusSub: Subscription = combineLatest([this.userOnline$, this.userActive$]).subscribe(([userOnline, userActive]) => {
+      this.isUserOnline = userOnline;
+      this.isUserActive = userActive;
+
+      if (userOnline && userActive) {
+        this.isStatusChanged = true;
+
+        this.onlineSubscription.unsubscribe();
+        this.activeSubscription.unsubscribe();
+
+        this.startOfflineTimer();
+        this.startInactiveTimer();
+        this.combineOfflineInactiveStatuses();
+      }
+    });
+    this.subscription.add(statusSub);
+  }
+
+  restartTimers(): void {
+    this.clearStatuses();
+    this.startTimers();
+  }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+    this.offlineSubscription.unsubscribe();
+    this.inactiveSubscription.unsubscribe();
   }
+
+  private startOnlineTimer(): void {
+    this.onlineSubscription = this.onlineTimer$.subscribe((value: number) => {
+      this.onlineOfflineTimerValue = value;
+
+      if (value === 0) {
+        this.userOnline$.next(true);
+      }
+    });
+  }
+
+  private startActiveTimer(): void {
+    this.activeSubscription = this.activeTimer$.subscribe((value: number) => {
+      this.activeInactiveTimerValue = value;
+
+      if (value === 0) {
+        this.userActive$.next(true);
+      }
+    });
+  }
+
+  private startOfflineTimer(): void {
+    this.offlineSubscription = this.offlineTimer$.subscribe((value: number) => {
+      this.onlineOfflineTimerValue = value;
+
+      if (value === 0) {
+        this.userOnline$.next(false);
+      }
+    });
+  }
+
+  private startInactiveTimer(): void {
+    this.inactiveSubscription = this.inactiveTimer$.subscribe((value: number) => {
+      this.activeInactiveTimerValue = value;
+
+      if (value === 0) {
+        this.userActive$.next(false);
+      }
+    });
+  }
+
+  private combineOfflineInactiveStatuses(): void {
+    const statusSub: Subscription = combineLatest([this.offlineTimer$, this.inactiveTimer$]).subscribe(([offlineTimerValue, inactiveTimerValue]) => {
+      if (offlineTimerValue === 0 && inactiveTimerValue === 0) {
+        this.showRestartButton = true;
+      }
+    });
+    this.subscription.add(statusSub);
+  }
+
+  private clearStatuses(): void {
+    this.isStatusChanged = false;
+    this.showRestartButton = false;
+  }
+}
+
+function getTimer(startDue = 0, interval = 1000): Observable<number> {
+  return timer(startDue, interval).pipe(
+    map((value) => 9 - value),
+    take(10)
+  )
 }
