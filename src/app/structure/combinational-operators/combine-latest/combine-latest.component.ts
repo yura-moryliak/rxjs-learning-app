3ca23f2942bb0514f28c3af213754b390a4497a5
ymrlk <y.moryliak@loginvsi.com>
2025-05-11 18:48:03 +0300

    Add CombineLatest Task 4 implementation
    Implemented CombineLatest Task 4 to combine cat images and todos using an API call mechanism with `combineLatest`. Included a new component with HTML, SCSS, and logic to display merged data, and updated parent component to include this task.

--- src/app/structure/combinational-operators/combine-latest/combine-latest.component.ts
+++ src/app/structure/combinational-operators/combine-latest/combine-latest.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import {CombineLatestTask1Component} from './tasks/combine-latest-task-1/combine-latest-task-1.component';
 import {CombineLatestTask2Component} from './tasks/combine-latest-task-2/combine-latest-task-2.component';
 import {CombineLatestTask3Component} from './tasks/combine-latest-task-3/combine-latest-task-3.component';
+import {CombineLatestTask4Component} from './tasks/combine-latest-task-4/combine-latest-task-4.component';
 
 @Component({
   selector: 'app-combine-latest',
   imports: [
     CombineLatestTask1Component,
     CombineLatestTask2Component,
-    CombineLatestTask3Component
+    CombineLatestTask3Component,
+    CombineLatestTask4Component
   ],
   templateUrl: './combine-latest.component.html',
   styleUrl: './combine-latest.component.scss'
--- src/app/structure/combinational-operators/combine-latest/tasks/combine-latest-task-4/combine-latest-task-4.component.ts
+++ src/app/structure/combinational-operators/combine-latest/tasks/combine-latest-task-4/combine-latest-task-4.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AsyncPipe} from '@angular/common';
+import {Subscription, combineLatest, timer, mergeMap, Observable, map} from 'rxjs';
+import {TaskOverviewComponent} from '../../../../../common/task-overview/task-overview.component';
+
+interface CatInterface {
+  id: number;
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface TodoInterface {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-combine-latest-task-4',
-  imports: [],
+  imports: [
+    TaskOverviewComponent,
+    AsyncPipe
+  ],
   templateUrl: './combine-latest-task-4.component.html',
   styleUrl: './combine-latest-task-4.component.scss'
 })
-export class CombineLatestTask4Component {
+export class CombineLatestTask4Component implements OnDestroy {
+  private readonly httpClient: HttpClient = inject(HttpClient);
+  private readonly subscription: Subscription = new Subscription();
 
+  private readonly catsApiUrl: string = 'https://api.thecatapi.com/v1/images/search?limit=4';
+  private readonly todosApiUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+
+  private readonly catsSource$ = timer(5000).pipe(
+    mergeMap(() => this.httpClient.get<CatInterface[]>(this.catsApiUrl))
+  )
+  private readonly todosSource$ = timer(8000).pipe(
+    mergeMap(() => this.httpClient.get<TodoInterface[]>(this.todosApiUrl))
+  );
+
+  catsAndTodos$: Observable<{ cats: CatInterface[], todos: TodoInterface[] }> = combineLatest([this.catsSource$, this.todosSource$]).pipe(
+    map(([cats, todos]) => ({
+      cats: cats,
+      todos: todos,
+    }))
+  )
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
