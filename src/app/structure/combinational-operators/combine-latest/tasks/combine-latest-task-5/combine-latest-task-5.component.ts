b8827f07763907b5bde72ae8be7670a60733de1f
ymrlk <y.moryliak@loginvsi.com>
2025-05-13 21:56:06 +0300

    Add conditional filtering logic to Task 5 component
    Implemented `combineLatest` with conditional filtering for Task 5, displaying results only when both values meet criteria. Updated related HTML for clarity and routing for combinational operators. Adjusted dynamic stream logic and added number display functionality.

--- src/app/structure/combinational-operators/combine-latest/tasks/combine-latest-task-5/combine-latest-task-5.component.ts
+++ src/app/structure/combinational-operators/combine-latest/tasks/combine-latest-task-5/combine-latest-task-5.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {Subscription} from 'rxjs';
+import {interval, map, of, Subscription, combineLatest, filter} from 'rxjs';
 import {TaskBaseComponent} from '../../../../task.base';
 import {TaskOverviewComponent} from '../../../../../common/task-overview/task-overview.component';
 
@@ -14,7 +14,20 @@ import {TaskOverviewComponent} from '../../../../../common/task-overview/task-ov
 export class CombineLatestTask5Component extends TaskBaseComponent implements OnInit, OnDestroy {
   private readonly subscription: Subscription = new Subscription();
 
+  staticLIstOfNumbers$ = of(5, 10, 15, 20);
+  dynamicListOfNumbers$ = interval(1000).pipe(map((val) => val * 5));
+
+  number1: number | undefined;
+  number2: number | undefined;
+
   ngOnInit(): void {
+    const sub = combineLatest([this.staticLIstOfNumbers$, this.dynamicListOfNumbers$]).pipe(
+      filter(([staticList, dynamicList]) => staticList > 10 && (dynamicList > 10 && dynamicList <= 45)),
+    ).subscribe(([num1, num2]) => {
+      this.number1 = num1;
+      this.number2 = num2;
+    });
+    this.subscription.add(sub);
   }
 
   ngOnDestroy(): void {
